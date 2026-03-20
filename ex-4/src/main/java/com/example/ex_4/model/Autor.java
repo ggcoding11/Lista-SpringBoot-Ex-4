e8750a162411fe173cc05d7aec95852d4913e4de
JeeJ <gustavogomescoding@gmail.com>
2026-03-20 08:43:43 -0300

    add-model

---
+++ ex-4/src/main/java/com/example/ex_4/model/Autor.java
@@ -0,0 +1,30 @@
+package com.example.ex_4.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_autor")
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "coluna_nome")
+    private String nome;
+
+    @Column(name = "coluna_nacionalidade")
+    private String nacionalidade;
+
+    @Column(name = "coluna_data_nascimento")
+    private LocalTime dataNascimento;
+}
